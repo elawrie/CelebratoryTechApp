006921d5ee12e290dd38f1f771a6a53d564c1f27
Evelyn Lawrie <lawrie@chapman.edu>
2024-03-05 17:36:15 -0800

    adds working match and data to DB

--- match_algo/match.py
+++ match_algo/match.py
@@ -26,4 +26,67 @@ for j in range(20):
         groups[rand_num].append(j)
         generated[rand_num] += 1
 
-print("Groups:", groups)
+print("Groups Round 1:", groups)
+
+# import the data from the database 
+
+# Round 2
+# based on MIXED communication styles (across the spectrum of different answers)
+
+
+
+
+
+
+
+# Round 3
+# based on CLOSEST matches (participants who are the most similar in their answers)
+
+# EXAMPLE OF DATA: {"001": ["002","004","007"], "002": ["003","006","019"] }
+top_matches = {}
+
+# group the three closest nodes to each node in arrays
+for user_id in queries:
+    top_matches[user_id] = QUERY RESPONSE 
+
+# find the frequencies of all nodes (how often they appear in top 3 groupings)
+
+# key = ID, value = hits in the top matches dictionary 
+# EXAMPLE OF DATA: {"004": 4}
+
+frequencies = {}
+for value in top_matches.values():
+    # loop through all indices in array 
+    for element in value:
+        # increment value if already in dictionary
+        if element in frequencies.keys():
+            frequencies[element] += 1
+        # add value if not already found in ductionary 
+        else:
+            frequencies[element] = 1
+
+# find the top 4 MOST present nodes
+sorted_dict = dict(sorted(frequencies.items(), key=lambda item: item[1], reverse=True))
+
+top_four = dict(sorted_dict[:4])
+
+# assign all top nodes to different groups
+
+groups = []
+
+
+
+# find unique nodes in each of their lists and assign them to their own groups
+
+# group all the nodes by their connection to the central nodes 
+top_connections = {}
+
+
+
+for value in top_matches.keys():
+    for key in top_four.keys():
+        if 
+
+# skip over duplicates
+
+# assign stragglers and duplicates 
